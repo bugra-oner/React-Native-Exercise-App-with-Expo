8a8bec342d23d102e1f824af227073eb38c87b32
buqraoner <boletroy@gmail.com>
2023-07-31 00:37:11 +0300

    save

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,12 +1,26 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View,Pressable } from 'react-native'
 import React from 'react'
+import { StatusBar } from 'expo-status-bar';
 
-export default function Home() {
+export default function Home({navigation}) {
   return (
-    <View>
+    
+    <View style={styles.container}>
       <Text>Home</Text>
+      <Pressable style={styles.button} onPress={() => navigation.navigate('Workout')} >
+      <Text>asdfasdfasdfasdfasdfasfasfasfas</Text>
+      </Pressable>
     </View>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container:{
+        flex :1,
+        marginTop: 125
+    },
+    button:{
+        height: 100,
+        width: 200,
+    },
+})
