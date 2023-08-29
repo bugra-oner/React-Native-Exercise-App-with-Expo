7fa73ca812555a21b277da164435cc979f8bbfa7
buqraoner <buqraoner@gmail.com>
2023-08-29 18:50:19 +0300

    save

--- App.jsx
+++ App.jsx
@@ -5,17 +5,13 @@ import BottomStackNavigator from './src/navigation/BottomNavigation';
 import { SafeAreaView } from 'react-native';
 
 import { NavigationContainer } from '@react-navigation/native';
+import AnimatedLottieView from 'lottie-react-native';
 
 import  {navigationRef} from './src/navigation/navigationRef'
 
 
-
-
 import { I18nextProvider } from 'react-i18next';
 import i18n,{useSetLanguage} from './src/i18n/i18n';
-
-
-
 
 
 export default function App() {
