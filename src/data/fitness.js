c37d900b298335e313dbc842a6d8c6df28078d09
buqraoner <buqraoner@gmail.com>
2023-09-25 23:32:17 +0300

    save

--- src/data/fitness.js
+++ src/data/fitness.js
@@ -20,7 +20,7 @@ export default [
          {
              id:"12",
              image: require('./DIAMOND_PUSHUP_MOTIFIED.gif'),
-             name:"INCLINED PUSH-UPS",
+             name:"DIAMOND PUSH UPS",
              sets:10,
          },
          {
@@ -51,31 +51,31 @@ export default [
       excersises : [
         {
           id:"90",
-          image:"https://media1.popsugar-assets.com/files/thumbor/f2sbzQY1h1zqiGEV9Mhr1IAcFMU/fit-in/2048xorig/filters:format_auto-!!-:strip_icc-!!-/2017/03/13/796/n/1922729/19cf2a4428446429_EXAMPLE.crossjacks.gif",
+          image:require('./19cf2a4428446429_EXAMPLE.crossjacks.gif'),
           name:"JUMPING JACKS",
           sets:12,
         },
         {
           id:"91",
-          image:"https://i.pinimg.com/originals/18/27/be/1827be178c019b1dc6f8a8d8b4a7b0b8.gif",
+          image:require('./1827be178c019b1dc6f8a8d8b4a7b0b8.gif'),
           name:"MOUNTAIN CLIMBERS",
           sets:10,
         },
         {
           id:"92",
-          image:"https://i.pinimg.com/originals/f4/b0/f3/f4b0f3093fcadd64968e4c46d1767b50.gif",
+          image:require('./f4b0f3093fcadd64968e4c46d1767b50.gif'),
           name:"HEEL TOUCH",
           sets:20,
         },
         {
           id:"94",
-          image:"https://i.pinimg.com/originals/cf/b5/67/cfb5677a755fe7288b608a4fec6f09a0.gif",
+          image:require('./plank.gif'),
           name:"PLANK",
           sets:10,
         },
         {
           id:"95",
-          image:"https://www.gymguider.com/wp-content/uploads/2017/10/straight-leg-raise.gif",
+          image:require('./straight-leg-raise.gif'),
           name:"LEG RAISES",
           sets:14,
         }
@@ -89,44 +89,44 @@ export default [
       excersises:[
         {
           id:"70",
-          image:"https://post.healthline.com/wp-content/uploads/2020/06/400x400_How_to_do_Zac_Efrons_Baywatch_Workout_Dumbbell_Lateral_Raise.gif",
+          image:require("./arm-raises.gif"),
           name:"ARM RAISES",
           sets:8,
         },
         {
           id:"71",
-          image:"https://thumbs.gfycat.com/CompleteZigzagFossa-max-1mb.gif",
+          image:require('./triceps-dips.gif'),
           name:"TRICEP DIPS",
-          sets:10,
+          sets:12,
         },
         {
           id:"72",
-          image:"https://thumbs.gfycat.com/MisguidedAridAlaskanmalamute-max-1mb.gif",
-          name:"DIAMOND_PUSHUP",
+          image: require('./DIAMOND_PUSHUP_MOTIFIED.gif'),
+          name:"DIAMOND PUSHUP",
           sets:10,
         },
         {
           id:"73",
-          image:"https://c.tenor.com/gI-8qCUEko8AAAAC/pushup.gif",
+          image:require('./pushup.gif'),
           name:"PUSH-UPS",
           sets:10,
         },
         {
           id:"74",
-          image:"https://i.pinimg.com/originals/8c/53/27/8c532774e4e1c524576bf1fb829ad895.gif",
+          image: require('./dumbell_curl.gif'),
           name:"DUMBELL CURL",
           sets:11,
         
         },
         {
           id:"75",
-          image:"https://www.vissco.com/wp-content/uploads/animation/sub/inch-worm.gif",
+          image:require('./inchworm.gif'),
           name:"INCH WORMS",
           sets:5,
         },
         {
           id:"76",
-          image:"https://c.tenor.com/jqwaOmRs-7gAAAAC/tricep-kick-back-tricep.gif",
+          image: require('./tricep-kick-back.gif'),
           name:"TRICEP LIFT",
           sets:8,
         }
@@ -140,37 +140,37 @@ export default [
       excersises: [
         {
           id:"20",
-          image:"https://i.pinimg.com/originals/ff/cf/40/ffcf40474f0758dfedebc823f5532aa1.gif",
+          image:require('./decline-pushups.gif'),
           name:"DECLINE PUSH-UPS",
-          sets:9,
+          sets:12,
         },
         {
           id:"21",
-          image:"https://image.2bstronger.com/article/fitness/the-14-toughest-do-anywhere-workout-moves-56348/1006.gif",
+          image:require('./hinduPushups.gif'),
           name:"HINDU PUSH-UPS",
-          sets:10,
+          sets:5,
         },
         {
           id:"22",
-          image:"https://thumbs.gfycat.com/TheseRigidBorer-size_restricted.gif",
+          image:require('./shoulderStrech.gif'),
           name:"SHOULDER STRETCH",
           sets:5,
         },
         {
           id:"23",
-          image:"https://thumbs.gfycat.com/AlertAfraidAldabratortoise-max-1mb.gif",
-          name:"COBRA STRETCH",
+          image:require('./hinduPushups.gif'),
+          name:"Hindu Push ups",
           sets:4,
         },
         {
           id:"25",
-          image:"https://media4.popsugar-assets.com/files/thumbor/BaWEAcCjtJEjiwf3PqJHnZ_S23A/fit-in/2048xorig/filters:format_auto-!!-:strip_icc-!!-/2016/08/10/766/n/1922729/1eae2dcf3d395379_PushUpTwist.gif",
+          image:require('./pushupRotation.gif'),
           name:"PUSH-UP & ROTATION",
           sets:12,
         },
         {
           id:"26",
-          image:"https://media3.popsugar-assets.com/files/thumbor/0Xiqpo7pxrKz5CKcRl7XYrKegko/fit-in/1024x1024/filters:format_auto-!!-:strip_icc-!!-/2014/02/27/847/n/1922729/1baf9ec0f5ce4ea9_burpee.3.gif",
+          image:require('./burpess.gif'),
           name:"BURPEES",
           sets:10
         }
