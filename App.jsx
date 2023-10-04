df80992e2f0c4483c378bcaeb706ccbdc58ecf98
Buğra Öner <boletroy@gmail.com>
2023-10-04 22:42:24 +0300

    save

--- App.jsx
+++ App.jsx
@@ -1,38 +1,36 @@
 // App.js
 
-import React from 'react';
-import BottomStackNavigator from './src/navigation/BottomNavigation';
-import { SafeAreaView } from 'react-native';
+import React from "react";
+import BottomStackNavigator from "./src/navigation/BottomNavigation";
+import { SafeAreaView } from "react-native";
 
-import { NavigationContainer } from '@react-navigation/native';
-import  {navigationRef} from './src/navigation/navigationRef'
+import { NavigationContainer } from "@react-navigation/native";
+import { navigationRef } from "./src/navigation/navigationRef";
 
-
-import { I18nextProvider } from 'react-i18next';
-import i18n,{useSetLanguage} from './src/i18n/i18n';
-import FlashMessage from 'react-native-flash-message';
+import { I18nextProvider } from "react-i18next";
+import i18n, { useSetLanguage } from "./src/i18n/i18n";
+import FlashMessage from "react-native-flash-message";
 
 // Context
-import { FitnessContext } from './src/Context';
+import { FitnessContext } from "./src/Context";
 
 export default function App() {
   const isLanguageLoaded = useSetLanguage();
- 
-  if(!isLanguageLoaded){
+
+  if (!isLanguageLoaded) {
     return null;
   }
 
   return (
     <I18nextProvider i18n={i18n}>
-    <FitnessContext>
-    <SafeAreaView style={{flex: 1, backgroundColor: 'white' }}>
-    <NavigationContainer
-     ref={navigationRef}>
-     <BottomStackNavigator />
-     </NavigationContainer>
-     <FlashMessage position="top" />
-     </SafeAreaView>
-         </FitnessContext>
-         </I18nextProvider>
+      <FitnessContext>
+        <SafeAreaView style={{ flex: 1, backgroundColor: "white" }}>
+          <NavigationContainer ref={navigationRef}>
+            <BottomStackNavigator />
+          </NavigationContainer>
+          <FlashMessage position="top" />
+        </SafeAreaView>
+      </FitnessContext>
+    </I18nextProvider>
   );
 }
