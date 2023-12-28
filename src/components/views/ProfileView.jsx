567f10d0f036ef4cbf5e1b152ec2581252c4114f
Buğra Öner <boletroy@gmail.com>
2023-12-28 19:55:09 +0300

    fix

--- src/components/views/ProfileView.jsx
+++ src/components/views/ProfileView.jsx
@@ -4,10 +4,7 @@ import { wp, hp, fp } from "../../utils";
 
 import CustomIcon from "../CustomIcon";
 
-
 export default function ProfileView({ name, gender, fix, onPress }) {
-
-  
   return (
     <View style={styles.container}>
       <View style={styles.TextView}>
@@ -26,7 +23,7 @@ export default function ProfileView({ name, gender, fix, onPress }) {
 const styles = StyleSheet.create({
   container: {
     flexDirection: "row",
-    backgroundColor: "#F3F5FF",
+    backgroundColor: "#ffffff",
     padding: 18,
     justifyContent: "space-between",
     width: wp(90),
@@ -41,15 +38,26 @@ const styles = StyleSheet.create({
     shadowOffset: { width: 2.3208664655685425, height: 2.3208664655685425 }, // Gölgenin konumunu burada belirleyebilirsiniz
     elevation: 10, // Sadece Android için shadow (iOS bu prop'u kullanmaz)
   },
-  TextView: {},
-  titleText: { fontSize: fp(2.8) },
-  genderText: { fontSize: fp(2.5) },
+  TextView: {
+    backgroundColor: "#ffffff",
+    padding: hp(2),
+    borderRadius: 6,
+    borderColor: "black",
+    backgroundColor: "#ffffff29",
+    shadowColor: "#000000", // Gölgenin rengini burada belirleyebilirsiniz
+    shadowOpacity: 0.6, // Gölgenin opaklığını burada belirleyebilirsiniz
+    shadowRadius: 7.604332447052002, // Gölgenin yuvarlaklık derecesini burada belirleyebilirsiniz
+    shadowOffset: { width: 2.3208664655685425, height: 2.3208664655685425 }, // Gölgenin konumunu burada belirleyebilirsiniz
+    elevation: 2, // Sadece Android için shadow (iOS bu prop'u kullanmaz)
+  },
+  titleText: { fontSize: fp(2.4) },
+  genderText: { fontSize: fp(2.2) },
   buttonText: {
     padding: 10,
   },
   touchableOpacity: {
     textAlign: "center",
-    backgroundColor: "#F3F5FF",
+    backgroundColor: "#f3f5ffd6",
     borderRadius: 7,
     borderColor: "black",
     shadowColor: "#000000", // Gölgenin rengini burada belirleyebilirsiniz
